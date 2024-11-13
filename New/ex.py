d727ceece275e053db204cf389402012399ace45
Vinhtheripper <quvinh1305@gmail.com>
2024-11-14 06:33:37 +0700

    chấm

---
+++ New/ex.py
@@ -0,0 +1,37 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(305, 292)
+        MainWindow.setStyleSheet("background-image: url(\'/Users/quangvinh/Documents/Pythonfile/thiencooksdinner/New/images/anh1.png\'); \n"
+                                 "background-repeat: no-repeat;      \n"
+                                 "background-position: center;      \n"
+                                 "background-size: cover;")
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 305, 33))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+# Tạo một file chạy ứng dụng ví dụ
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
---
+++ New/images/Mainwindow.py
@@ -0,0 +1,6 @@
+from PyQt6.QtWidgets import QApplication
+from untitled import Ui_MainWindow
+app = QApplication([])
+myWindow = Ui_MainWindow()
+myWindow.show()
+app.exec()
